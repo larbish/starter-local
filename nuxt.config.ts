12c2d318b2777cb9646731cbb684980166eaa87c
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-09-27 15:27:40 +0200

    feat(icon): test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthq/studio', '@nuxt/icon'],
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  studio: {
+    enabled: true,
+    gitInfo: {
+      name: 'starter-local',
+      owner: 'larbish',
+      url: 'https://balek.io'
+    }
+  },
+
+  hooks: {
+    'components:extend': (components) => {
+      const iconComponent = components.find(c => { console.log(c); c.pascalName === 'icon'})
+      if (iconComponent) {
+        iconComponent.global = true
+      }
+    },
+  },
+
+  compatibilityDate: '2024-09-16'
 })
